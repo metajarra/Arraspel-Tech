96c12dd27e0cd89cfb1d8f6bd99f4abfad23f025
John Sangster <john.sangster445@gmail.com>
2024-03-06 19:38:33 -0500

    Update console_graph_test.gd

--- Arraspel Tech/Console Graphs/console_graph_test.gd
+++ Arraspel Tech/Console Graphs/console_graph_test.gd
@@ -60,6 +60,30 @@ func stacked_graph_bar(amounts : Array, descriptions : Array = []) -> String: # 
 	
 	return output
 
+func _stacked_graph_bar_indexed(amounts : Array) -> String:
+	# example format:
+	# 1  2      3 4   5 6                  7
+	# :::=======::====::===================:::::
+
+	var numbers : String
+	var bar : String
+	
+	for index in amounts.size():
+		var indicator_number := "%s" % index
+		numbers += indicator_number
+		
+		var fill_character := ":"
+		if index % 2 == 1:
+			fill_character = "="
+		
+		for n in amounts[index]:
+			bar += fill_character
+			
+			if n >= indicator_number.length(): 
+				numbers += " "
+	
+	return numbers + "\n" + bar
+
 func stacked_graph_bar_simple(amounts : Array) -> String: # no labels
 	# example format:
 	# :::=======::====::===================:::::
@@ -76,3 +100,36 @@ func stacked_graph_bar_simple(amounts : Array) -> String: # no labels
 			bar += fill_character
 	
 	return bar
+
+func percentage_bar(amounts : Array, descriptions : Array = []) -> String: # no labels
+	
+	var total := 0
+	for item in amounts:
+		total += item
+	
+	var amounts_as_percentages := amounts
+	for index in amounts.size():
+		var adjusted_amount : float = amounts[index] as float / total * 100
+		amounts_as_percentages[index] = adjusted_amount
+	
+	var output : String
+	
+	output += _stacked_graph_bar_indexed(amounts_as_percentages)
+	for index in descriptions.size():
+		output += ("\n%s " % index) \
+		+ "%0.1f" % amounts_as_percentages[index] + '% ' \
+		+ descriptions[index]
+	
+	return output
+
+func percentage_bar_simple(amounts : Array) -> String: # no labels
+	var total := 0
+	for item in amounts:
+		total += item
+	
+	var amounts_as_percentages := amounts
+	for index in amounts.size():
+		var adjusted_amount : int = amounts[index] as float / total * 100
+		amounts_as_percentages[index] = adjusted_amount
+	
+	return stacked_graph_bar_simple(amounts_as_percentages)
